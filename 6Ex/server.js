ef575b89e573cf53fdf8167a697de70010a1c9fa
Namasivaayam-L <namasivaayam007vijay@gmail.com>
2023-02-27 23:41:22 +0530

    Completed 5Ex

---
+++ 6Ex/server.js
@@ -0,0 +1,4 @@
+require('mongoose').connect('mongodb://localhost:27017/local');
+// const MyModel = mongoose.model('Test', new Schema({ name: String }));
+// // Works
+// MyMode.findOne(function(error, result) { /* ... */ });
