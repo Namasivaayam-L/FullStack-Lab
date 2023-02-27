c4e4b28bb402c730441c3d9f2bc4f6d8a5f7c333
Namasivaayam-L <namasivaayam007vijay@gmail.com>
2023-02-28 00:15:32 +0530

    Completed 6Ex

--- 6Ex/server.js
+++ 6Ex/server.js
@@ -1,4 +1,29 @@
-require('mongoose').connect('mongodb://localhost:27017/local');
-// const MyModel = mongoose.model('Test', new Schema({ name: String }));
-// // Works
-// MyMode.findOne(function(error, result) { /* ... */ });
+const router = require('express')()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/FullStack-Lab');
+
+const Feedback = mongoose.model('Feedback-6Ex', new mongoose.Schema({
+    name: String,
+    rollno: String,
+    phno: Number,
+    survey: Object
+}));
+
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
+router.listen(3000,()=>console.log('Server Fired Up at 3000'))
+
+router.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html')
+})
+
+router.post('/sendFeedback', async(req, res) => {
+    let { name, rollno, phno, ...survey } = req.body
+    // console.log(name,rollno,phno,survey);
+    await new Feedback({name,rollno,phno,survey}).save()
+    // console.log(await Feedback.find());
+})
